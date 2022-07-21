012093ab47d60adfeb8d9d46c169668b73d373e5
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-21 11:09:20 +0200

    Classlist

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -1,11 +1,17 @@
-const allLinks = document.querySelectorAll("a");
+// const allLinks = document.querySelectorAll("a");
 
 // for (let link of allLinks) {
 //   link.innerText = "I AM A LINK!!!";
 // }
 
-for (let link of allLinks) {
-  link.style.color = "rgb(0, 108, 134)";
-  link.style.textDecorationColor = "magenta";
-  link.style.textDecorationStyle = "wavy";
-}
+// for (let link of allLinks) {
+//   link.style.color = "rgb(0, 108, 134)";
+//   link.style.textDecorationColor = "magenta";
+//   link.style.textDecorationStyle = "wavy";
+// }
+
+const h2 = document.querySelector("h2");
+
+h2.classList.add("purple", "border");
+// h2.classList.remove("border");
+// h2.classList.toggle("purple");
